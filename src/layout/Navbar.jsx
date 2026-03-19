e0d244e9b526e988d54a0a35b4e4955ae8bc9653
Learan-Canning <learancanning@mail.com>
2026-03-19 00:45:53 +0000

    add responsive navbar with button and mobile menu ico

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/Button";
+import { Menu } from "lucide-react";
+
 const navLinks = [ 
     {href: "#about", label: "About"},
     {href: "#projects", label: "Projects"},
@@ -13,7 +16,7 @@ export const Navbar = () => {
                     LC<span className="text-primary">.</span>
                 </a>
                 {/* Desktop Nav */}
-                <div className="flex items-center gap-1">
+                <div className="hidden md:flex items-center gap-1">
                   <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                     {navLinks.map((link, index) => (
                         <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">
@@ -22,7 +25,20 @@ export const Navbar = () => {
                     ))}
                   </div>
                 </div>
+                {/* CTA Button */}
+                <div className="hidden md:block">
+                    <Button size="sm">Contact Me</Button>
+                </div>
+
+                {/* Mobile Menu Button */}
+                <button className="md:hidden p-2 text-foreground">
+                    <Menu size={24} />
+                </button>
             </nav>
+            
+            {/* Mobile Menu */}
+            <div  className="md:hidden glass"></div>
+
         </header>
     );
 };
